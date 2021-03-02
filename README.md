182c94aad281067ef44638fa317e98a746151109
Mateusz <m_paczocha@o2.pl>
2021-03-02 21:50:43 +0100

    Final commit with modified README and source files

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 designcourse-portfolio
 
-Technologies: HTML5, CSS3, SCSS.
+Technologies: HTML5, CSS3 (SCSS), GSAP (javascript animation library).
 
 Install template (“dist” + “src” folder structure) (npm i). Run the “gulp” command in terminal. We do all the work (file editing) in the src directory. Based on it, the page in the “dist” directory is built. The only thing is to manually add images to “dist/images” folder, because html, js and css are generated automatically.
 
-Project based on the "Designing & Building a Personal Portfolio from SSCRATCH!" from DesignCourse YouTube channel.
+Project based on the "Designing & Building a Personal Portfolio from SCRATCH!" from DesignCourse YouTube channel.
---
+++ src/js/app.js
@@ -0,0 +1,63 @@
+const { black } = require("ansi-colors");
+
+gsap.registerPlugin(ScrollTrigger);
+var tl = gsap.timeline();
+
+tl.from(".content", {
+  y: "-30%",
+  opacity: 0,
+  duration: 2,
+  ease: Power4.easeOut,
+});
+
+tl.from(
+  ".stagger1",
+  {
+    opacity: 0,
+    y: -50,
+    stagger: 0.3,
+    ease: Power4.easeOut,
+    duration: 2,
+  },
+  "-=1.5"
+);
+
+tl.from(
+  ".hero-design",
+  {
+    opacity: 0,
+    y: 50,
+    ease: Power4.easeOut,
+    duration: 1,
+  },
+  "-=2"
+);
+
+gsap.from(".square-anim", {
+  stagger: 0.2,
+  scale: 0.1,
+  duaration: 1,
+  ease: Back.easeOut.config(1.7),
+});
+
+gsap.from(".transition2", {
+  scrollTrigger: {
+    trigger: ".transition2",
+    start: "top bottom",
+  },
+  y: 50,
+  opacity: 0,
+  duration: 1.2,
+  stagger: 0.3,
+});
+
+gsap.from(".transition3", {
+  scrollTrigger: {
+    trigger: ".transition3",
+    start: "top center",
+  },
+  y: 50,
+  opacity: 0,
+  duration: 1.2,
+  stagger: 0.6,
+});
